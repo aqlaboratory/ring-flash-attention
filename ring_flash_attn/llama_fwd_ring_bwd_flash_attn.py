f70507113eba136cc245c8e9dd081e6b0fa32027
Eric Wong <eric_wong@live.ca>
2024-12-16 02:53:34 -0500

    try llama fwd and bwd debug off

--- ring_flash_attn/llama_fwd_ring_bwd_flash_attn.py
+++ ring_flash_attn/llama_fwd_ring_bwd_flash_attn.py
@@ -330,8 +330,8 @@ def llama3_flash_attn_func(
                               dtype=torch.int32, device=k.device)
     (cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k, local_k_slice
     ) = llama3_flash_attn_prepare_cu_seqlens(cu_seqlens, causal, rank, world_size)
-    logging.debug(f"{cu_seqlens_q}, {cu_seqlens_k}, {max_seqlen_q}, {max_seqlen_k}, {local_k_slice}")
-    logging.debug(f"{cu_seqlens}, {causal}, {rank}, {world_size}")
+    # logging.debug(f"{cu_seqlens_q}, {cu_seqlens_k}, {max_seqlen_q}, {max_seqlen_k}, {local_k_slice}")
+    # logging.debug(f"{cu_seqlens}, {causal}, {rank}, {world_size}")
     q, k, v = [rearrange(t, 'b s h d -> (b s) h d') for t in (q, k, v)]
     # k = k.contiguous().view(-1,  nheads_k, head_dim)
     # v = v.contiguous().view(-1,  nheads_k, head_dim)
