8d0029d6e1726e9e57f140fa283ffbcc35056a68
Eric Wong <eric_wong@live.ca>
2024-12-20 05:54:07 -0500

    debug

--- ring_flash_attn/llama_fwd_ring_bwd_flash_attn.py
+++ ring_flash_attn/llama_fwd_ring_bwd_flash_attn.py
@@ -508,7 +508,7 @@ def llama3_flash_attn_varlen_custom_func(
     ) = llama3_flash_attn_prepare_cu_seqlens(cu_seqlens, causal, rank, world_size)
     # logging.debug(f"{cu_seqlens_q}, {cu_seqlens_k}, {max_seqlen_q}, {max_seqlen_k}, {local_k_slice}")
     # logging.debug(f"{cu_seqlens}, {causal}, {rank}, {world_size}")
-    q, k, v = [rearrange(t, 'b s h d -> (b s) h d') for t in (q, k, v)]
+    # q, k, v = [rearrange(t, 'b s h d -> (b s) h d', b=) for t in (q, k, v)]
     # k = k.contiguous().view(-1,  nheads_k, head_dim)
     # v = v.contiguous().view(-1,  nheads_k, head_dim)
     # q = q.contiguous().view(-1,  nheads_k, head_dim)
