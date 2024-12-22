729d0b6318c09695ead0a5f148e87e0360b1a611
Eric Wong <eric_wong@live.ca>
2024-12-22 10:43:32 -0500

    fix llama bwd?

--- ring_flash_attn/ring_flash_attn.py
+++ ring_flash_attn/ring_flash_attn.py
@@ -115,8 +115,7 @@ def ring_flash_attn_backward(
                 "alibi_slopes": alibi_slopes,
                 "deterministic": deterministic,
             }
-            # logging.debug(params)
-            logging.debug(f"q {params['q'].shape} k {params['k'].shape} v {params['v'].shape} dout {params['dout'].shape} softmax_lse {params['softmax_lse'].shape}")            
+            # logging.debug(f"q {params['q'].shape} k {params['k'].shape} v {params['v'].shape} dout {params['dout'].shape} softmax_lse {params['softmax_lse'].shape}")            
             _flash_attn_backward(**params)
 
             if dq is None:
